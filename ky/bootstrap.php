8971b66991720bda37f20aea4a1a0878e1722066
KunYingCMS <cms@yujia.xyz>
2026-09-22 23:47:47 +0800

    ci: v1.0.24 新增全新安装E2E验证与router.php开发路由

--- ky/bootstrap.php
+++ ky/bootstrap.php
@@ -4,7 +4,7 @@
  * KunYing CMS
  */
 define('KY_PATH', __DIR__ . '/..');
-define('KY_VERSION', '1.0.23');
+define('KY_VERSION', '1.0.24');
 define('KY_RELEASE', 20260924);
 
 error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_WARNING);
